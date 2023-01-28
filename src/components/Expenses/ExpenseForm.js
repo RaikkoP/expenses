6d2ec7effcd2352b69fbe73b7cfca99bdcb52d71
RaikkoP <raikko.prants@voco.ee>
2023-01-28 17:59:34 +0200

    Vormi andmete saatmine töötlus

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -18,21 +18,34 @@ const ExpenseForm = (props) => {
        setEnteredDate(event.target.value)
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date : new Date(enteredDate)
+        }
+        console.log(expenseData)
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+    }
+
     return(
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
                     <input type="text"
-                    onChange={titleChangeHandler}/>
+                    onChange={titleChangeHandler} value={enteredTitle}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2023-01-18" max="2025-12-31" onChange={dateChangeHandler}/>
+                    <input type="date" min="2023-01-18" max="2025-12-31" onChange={dateChangeHandler} value={enteredDate}/>
                 </div>
             </div>
             <div className="new-expense__actions">
